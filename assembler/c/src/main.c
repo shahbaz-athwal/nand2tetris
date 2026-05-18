05e166bf894cac4c2c3ba9eb83e9c8bea7c71a48
Shahbaz Singh <shahbazathwal2107@gmail.com>
2026-05-18 16:49:00 -0300

    line cleaning

--- assembler/c/src/main.c
+++ assembler/c/src/main.c
@@ -1,6 +1,48 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    printf("Hack Assembler starting...\n");
-    return 0;
+void clean_line(char *buffer) {
+  int read = 0;
+  int write = 0;
+
+  while (buffer[read] != '\0') {
+    if (buffer[read] == '/' && buffer[read + 1] == '/') {
+      break;
+    }
+    if (buffer[read] != ' ' && buffer[read] != '\n' && buffer[read] != '\r' &&
+        buffer[read] != '\t') {
+      buffer[write] = buffer[read];
+      write++;
+    }
+    read++;
+  }
+
+  buffer[write] = '\0';
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Usage: %s <input_file>\n", argv[0]);
+    return 1;
+  }
+
+  FILE *file = fopen(argv[1], "r");
+  if (file == NULL) {
+    printf("Error: could not open file %s\n", argv[1]);
+    return 1;
+  }
+
+  char buffer[256];
+  while (fgets(buffer, sizeof(buffer), file)) {
+    clean_line(buffer);
+    printf("%s", buffer);
+    if (buffer[0] != '\0') {
+      putchar('\n');
+    }
+  }
+
+  fclose(file);
+
+  return 0;
 }
