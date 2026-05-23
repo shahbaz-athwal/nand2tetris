a014598a5503553224b8a1f693302d38c637d55f
Shahbaz Singh <shahbazathwal2107@gmail.com>
2026-05-23 19:17:01 -0300

    line filter callback

--- assembler/c/src/line-reader.c
+++ assembler/c/src/line-reader.c
@@ -3,15 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef void (*LineFilterFn)(char *line);
+
 typedef struct {
   FILE *fp;
   char *line_buffer;
   size_t buffer_size;
   ssize_t read_line_size;
   bool has_cached_line;
+  LineFilterFn filter;
 } LineReader;
 
-LineReader *lr_open(char *file_name) {
+LineReader *lr_open(char *file_name, LineFilterFn filter) {
   FILE *fp = fopen(file_name, "r");
   if (!fp) {
     return NULL;
@@ -28,6 +31,7 @@ LineReader *lr_open(char *file_name) {
   lr->buffer_size = 0;
   lr->read_line_size = -1;
   lr->has_cached_line = false;
+  lr->filter = filter;
 
   return lr;
 }
@@ -39,12 +43,16 @@ bool has_more_lines(LineReader *lr) {
   if (lr->has_cached_line)
     return true;
 
-  lr->read_line_size = getline(&lr->line_buffer, &lr->buffer_size, lr->fp);
-
-  if (lr->read_line_size != -1) {
-    lr->has_cached_line = true;
-    return true;
-  }
+  while ((lr->read_line_size =
+              getline(&lr->line_buffer, &lr->buffer_size, lr->fp)) != -1) {
+    if (lr->filter) {
+      lr->filter(lr->line_buffer);
+    }
+    if (lr->line_buffer[0] != '\0') {
+      lr->has_cached_line = true;
+      return true;
+    }
+  };
 
   return false;
 }
