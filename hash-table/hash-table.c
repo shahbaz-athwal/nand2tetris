a45e03a73fdc31a62964b6f80aa0a0ecfc004e28
Shahbaz Singh <shahbazathwal2107@gmail.com>
2026-05-25 21:39:30 -0300

    resize fn

--- hash-table/hash-table.c
+++ hash-table/hash-table.c
@@ -1,19 +1,24 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
+
+#define INITIAL_CAPACITY 16;
+#define LOAD_FACTOR 0.75f;
 
 typedef void (*FreeFnCb)(void *);
 
-typedef struct {
+typedef struct HTEntry {
   char *key;
   void *value;
-  struct HMEntry *next;
+  struct HTEntry *next;
 } HTEntry;
 
 typedef struct {
   HTEntry **buckets;
   size_t size;
   size_t capacity;
+  FreeFnCb free_callback;
 } HashTable;
 
 static uint32_t ht_hash(const char *key) {
@@ -23,4 +28,62 @@ static uint32_t ht_hash(const char *key) {
     hash *= 16777619u;
   }
   return hash;
+}
+
+HashTable *ht_create(FreeFnCb free_callback) {
+  HashTable *ht = malloc(sizeof(HashTable));
+  if (!ht)
+    return NULL;
+  ht->capacity = INITIAL_CAPACITY;
+  ht->free_callback = free_callback;
+  ht->size = 0;
+  ht->buckets = calloc(ht->capacity, sizeof(HTEntry *));
+  if (!ht->buckets) {
+    free(ht);
+    return NULL;
+  }
+
+  return ht;
+}
+
+void ht_destroy(HashTable *ht) {
+  if (!ht)
+    return;
+
+  for (int i = 0; i < ht->capacity; i++) {
+    HTEntry *node = ht->buckets[i];
+    while (node) {
+      HTEntry *next = node->next;
+      if (ht->free_callback) {
+        ht->free_callback(node->value);
+      }
+      free(node->key);
+      free(node);
+      node = next;
+    }
+  }
+  free(ht->buckets);
+  free(ht);
+}
+
+static bool ht_resize(HashTable *ht, size_t new_capacity) {
+  HTEntry **new_bucket = calloc(new_capacity, sizeof(HTEntry));
+  if (!new_bucket) {
+    return false;
+  }
+  for (int i = 0; i < ht->capacity; i++) {
+    HTEntry *node = ht->buckets[i];
+    while (node) {
+      HTEntry *next = node->next;
+      uint32_t idx = ht_hash(node->key) % new_capacity;
+      node->next = new_bucket[idx];
+      new_bucket[idx] = node;
+
+      node = next;
+    }
+  }
+  free(ht->buckets);
+  ht->buckets = new_bucket;
+  ht->capacity = new_capacity;
+  return true;
 }
